a46758124f13876c173c0101b757e8d8514403a5
salman9000 <salmanhanif133@gmail.com>
2021-11-12 15:15:51 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import { AiOutlineEdit } from "react-icons/ai";
+import "./css/styles.css"
 
-function App() {
+const App = () => {
+  const [inputTodo, setInputTodo] = useState("")
+  const [todoId, setTodoId] = useState(3)
+  const [todoArray, setTodoArray] = useState([
+    {id: 0, text: "Get Eggs"},
+    {id: 1, text: "Get Bread"},
+    {id: 2, text: "Go for a walk walking"}
+  ])
+
+  const addTodo = () => {
+    setTodoArray([...todoArray, {id: todoId, text: inputTodo}])
+    setTodoId((prevId) => prevId + 1)
+    setInputTodo("")
+  }
+
+  const removeFromTodo = (todoId) => {
+    setTodoArray(todoArray.filter(todo => todoId !== todo.id))
+  }
+
+  const editTodo = (newText, todoId) => {
+    const todoArrayCopy = [...todoArray]
+    const todoElement = todoArrayCopy.filter(todo => todoId === todo.id )[0]
+    todoElement.text = newText
+    // console.log(todoElement)
+    setTodoArray(todoArrayCopy)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>
+        <h1>What's the Plan for Today</h1>
+      <div className="todolist">
+        <div className="input-field">
+          <input placeholder="Add a todo" type="text" value={inputTodo} onChange={e => setInputTodo(e.target.value)} />
+          {inputTodo !== "" ? <button className="btn-form" onClick={() => addTodo()}>
+            Add Todo
+          </button> : 
+          <button className="btn-form btn-disable" disabled onClick={() => addTodo()}>
+          Add Todo
+        </button>}
+          
+        </div>
+        <div className="list">
+          {todoArray.length > 0 ? todoArray.map(todo => (
+            <div className="eachTodoCard" key={todo.id}>
+              <p className="todoText">{todo.text}</p>
+              <div className="icons-container">
+                <AiOutlineCloseCircle className="icon" onClick={() => removeFromTodo(todo.id)}/>
+                <AiOutlineEdit className="icon" onClick={() => editTodo(prompt("Edit text", `${todo.text}`), todo.id)} />
+              </div>
+            </div>
+          )) : <div><h1>Hmmm... here is nothing here.</h1><h1>Add an item to get started</h1></div>}
+        </div>
+      </div>
+      </div>
     </div>
   );
 }
